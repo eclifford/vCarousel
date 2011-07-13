611bffca0138c80c567dc2daa690136aa0b1db68
eclifford <ericgclifford@gmail.com>
2011-07-13 15:16:34 -0700

    Added auto transitions, swipe left, swipe right

--- carousel.js
+++ carousel.js
@@ -2,44 +2,43 @@
 	$.fn.vCarousel = function(options) {
 
 		var config = {
-			width: 400,
-			height: 400,
+			width: 320,
+			height: 372,
 			bullets: true,
 			buttons: true,
 			auto: true,
-			autoSpeed: 4000,
-			caption: true,
-			bulletIcon: "x",
-			prevButtonIcon: "prev.jpg",
-			nextButtonIcon: "next.jpg"
+			autoSpeed: 5000,
+			caption: true, // Not being used yet
+			bulletIcon: "bullet.png",
+			bulletIconActive: "bullet_a.png",
+			prevButtonIcon: "prev.png",
+			nextButtonIcon: "next.png"
 		}
 		
 		return this.each(function() {		
-			
-			// Setup private variables
-			var carousel = this;
+			// Private variables		
 			var $carousel = $(this);
-			var container, wrapper, items, numitems, caption;
+			var container, wrapper, items, numitems, caption, interval;
 			
 			if(options) {
-				$.extend(defaultOptions, options);
+				$.extend(config, options);
 			}
-							
-			// PRIVATE FUNCTIONS
+			
+			// Constructor 
 			var initialize = function() {
-				buildComponent();
+				setup();
 				handleInput();
+				update();
 				return this;
 			}
 			
-			var buildComponent = function() {
+			var setup = function() {
 				$carousel.children().wrapAll('<div class="vCarousel" />');
 				this.container = $carousel.find('.vCarousel');
-				this.container.find('div').wrapAll('<div class="wrapper" />')
+				this.container.find('div.item').wrapAll('<div class="wrapper" />')
 				this.wrapper = this.container.find('.wrapper');
 				this.items = this.wrapper.find('div.item');
-				
-				
+						
 				// Previous and next button
 				if(config.buttons) {
 					this.wrapper.append('<a href="#" class="prev"><img src="' + config.prevButtonIcon + '" /></a>' +
@@ -48,63 +47,79 @@
 				
 				this.wrapper.append('<div class="nav" />');
 				var nav = this.wrapper.find('.nav');
-							
-				this.container.append('<div class="caption" />');
-				this.caption = this.container.find('.caption');
-	
-	
+						
 				this.items.each(function(i) {
-					i += 1;
-					
+					i += 1;		
+							
 					if(config.bullets === true) {
 						nav.append('<img src="' + config.bulletIcon + '" class="bullet" />')				
-					}
-								
+					}		
+										
 				});
 								
 				this.wrapper.width(config.width).height(config.height);
 				this.items.hide().first().addClass('current').show();
-				$(".bullet", $carousel).first().addClass('current');
-				setCaption();
+				$(".bullet", $carousel).first().attr('src', config.bulletIconActive);
 			}
 			
-			var handleInput = function() {	
-						
-				$(this.wrapper.find('a.next').click(function(e) {		
+			var handleInput = function() {									
+				$(this).bind('swipeleft', function(e,ui) {
 					e.preventDefault();			  
 					slideNext();
-					setCaption();
-				}));
+					update();
+				});
 				
-				$(this.wrapper.find('a.prev').click(function(e) {		
+				$(this).bind('swiperight', function(e,ui) {
 					e.preventDefault();			  
 					slidePrev();
-					setCaption();
-				}));
+					update();
+				});
 				
+				$('a.next', $carousel).bind('tap', function(e,ui) {
+					e.preventDefault();			  
+					slideNext();
+					update();
+				});
+				
+				$('a.prev', $carousel).bind('tap', function(e,ui) {
+					e.preventDefault();			  
+					slidePrev();
+					update();
+				});
+							
 				this.wrapper.hover(function() {
 					$('a.next', $carousel).stop(true, true).fadeToggle();
 					$('a.prev', $carousel).stop(true, true).fadeToggle();
 				});
 			}
 			
+			
+			
+			var update = function() {
+				if(config.auto) {
+					clearInterval(this.interval);
+					this.interval = setInterval(function() {slideNext();}, config.autoSpeed)
+				}		
+			}
+			
 			var slideTo = function(i) {			
-					this.items
-						.removeClass('current')
-						.fadeOut('slow')
-						.eq(i)
-						.fadeIn('slow')
-						.addClass('current');
-					$(".bullet", $carousel)
-						.removeClass('current')
-						.eq(i)
-						.addClass('current');
+				$(".item", $carousel)
+					.removeClass('current')
+					.fadeOut('slow')
+					.eq(i)
+					.fadeIn('slow')
+					.addClass('current');
+				$(".bullet", $carousel)
+					.attr('src', config.bulletIcon)
+					.eq(i)
+					.attr('src', config.bulletIconActive);
 			}
 			
 			var slideNext = function() {
 				var index = getCurrentIndex();
-				if(index < this.items.length -1)
+				if(index < $("div.item", $carousel).length-1) {
 					slideTo(index+1);
+				}
 				else
 				{
 					slideTo(0);
@@ -118,22 +133,15 @@
 				}
 				else
 				{
-					slideTo(this.items.length -1);
+					slideTo($("div.item", $carousel).length-1);
 				}		
 			}
 			
-			var setCaption = function() {
-				var index = getCurrentIndex();
-				var content = this.items.eq(index).find("p").text();
-				this.caption.text(content);
-			}
-			
 			var getCurrentIndex = function() {
-				return this.items.filter('.current').index();
+				return $("div.item", $carousel).filter('.current').index();
 			}
 					
 			return initialize();
-
 		});
 	}
 	
